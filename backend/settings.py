7dd744f43560285d0c6e519c14d4703b422f47e5
creativeagent <stephan.kobel@creativeagent.ch>
2024-10-25 16:04:52 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -139,6 +139,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR/'static'
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
