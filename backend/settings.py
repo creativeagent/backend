19e3211b344eb9cdb2e28dd07122670bd2345bac
creativeagent <stephan.kobel@creativeagent.ch>
2024-10-25 15:51:14 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
+STATICFILES_DIRS = [BASE_DIR/'staticfiles',]
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
