17169185d6f2a7ca9d6db0bf15e859831867ff53
creativeagent <stephan.kobel@creativeagent.ch>
2024-10-25 15:29:04 +0200

    Change to Postgres

--- backend/settings.py
+++ backend/settings.py
@@ -137,3 +137,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
